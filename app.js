47dfc41ea729d4710b9a57a9ab8174cd74cf0655
mdlmdel <mdlmdel@gmail.com>
2016-12-10 14:16:11 -0500

    Update and format code

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
+
   // Submit event handler
   $('#search-form').submit( function(e) {
     e.preventDefault();
     getMostPopular(results);
   })
 
-  $('#read').hide();
-  // Get lists of most popular articles from the New York Times Magazine section 
+  // Hide the top stories button when the page loads
+  $('#summary').hide();
+
+  // Get lists of the most emailed articles within the last 30 days from the New York Times Magazine section
   function getMostPopular() {
     var url = 'https://api.nytimes.com/svc/mostpopular/v2/mostemailed/Magazine/30.json';
     let params = {
@@ -15,30 +18,29 @@ $(document).ready(function () {
     }
     url += '?' + $.param(params)
     $.ajax({
-      url: url, 
-      title: results.title,
-
+      url: url,
       method: 'GET',
     }).done(function(data) {
       console.log(data);
+
       // Define which results will be displayed and display them
       for (var i = 0; i < 5; i++) {
         var title = data.results[i].title;
-        
         $('#read').show();
         var article_url = data.results[i].url;
         var abstract = data.results[i].abstract;
+
         // Use bracket notation for media-metadata
         var image = data.results[i].media[0]['media-metadata'][0].url;
         console.log(image);
-
+        
+        // Display results in HTML
         $('#results').append("<li><h3>" + title + 
           "</h3>" + abstract + "<br><br>" + "<a target='blank' href='" + article_url + "'>" + 
           "<img src='" + image + "'>" + "'</a></li>");
-        $('#read').append("<a target='blank' href='" + article_url + "'>" + 
+        $('#summary').append("<a target='blank' href='" + article_url + "'>" + 
           "<img src='" + image + "'>" + "'</a>");
       } 
-      
     }).fail(function(err) {
       throw err;
     });
